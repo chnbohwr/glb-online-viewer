5a32ee85ea2215c10153488f31585afdec322d7c
hyman_chen <hyman_chen@xxentria-tech.com>
2021-09-29 10:55:23 +0800

    change far

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(800, 700, false);
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x444444);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 50000);
 camera.position.z = 25;
 
 const light = new THREE.AmbientLight(0xffffff);
